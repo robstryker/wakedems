bcb8e7f849bb2454247e4d8b6a6121a729c5fe82
Rob Stryker <rob@oxbeef.net>
2020-01-21 18:06:55 -0500

    Add a script
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ distribution/scripts/runPrecinctsWithDefinitions.sh
@@ -0,0 +1,1 @@
+ls ../resources/precincts/definitions/ | grep -v template | cut -f 1 -d "." | awk '{ print "echo \"Running precinct " $0 "\"; java -jar reports-0.1.0-SNAPSHOT.jar " $0 " > ../output/" $0 ".txt";}' | sh

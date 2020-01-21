d89279f3b5ffa1a1d9761747dbed35a8fa9fc5ac
Rob Stryker <rob@oxbeef.net>
2020-01-21 18:14:51 -0500

    bugfix

--- distribution/scripts/runPrecinctsWithDefinitions.sh
+++ distribution/scripts/runPrecinctsWithDefinitions.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+rm -rf ../output
+mkdir ../output
+
 ls ../resources/precincts/definitions/ | grep -v template | cut -f 1 -d "." | awk '{ print "echo \"Running precinct " $0 "\"; java -jar reports-0.1.0-SNAPSHOT.jar " $0 " > ../output/" $0 ".txt";}' | sh

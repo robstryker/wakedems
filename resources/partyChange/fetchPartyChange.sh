d1060b0868d9eacc8e92364607e0c6efc95d6503
Rob Stryker <rob@oxbeef.net>
2025-03-23 23:21:07 -0400

    Small updates
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- resources/partyChange/fetchPartyChange.sh
+++ resources/partyChange/fetchPartyChange.sh
@@ -4,3 +4,7 @@ wget https://s3.amazonaws.com/dl.ncsbe.gov/data/PartyChange/2019_party_change_li
 wget https://s3.amazonaws.com/dl.ncsbe.gov/data/PartyChange/2020_party_change_list.csv
 wget https://s3.amazonaws.com/dl.ncsbe.gov/data/PartyChange/2021_party_change_list.csv
 wget https://s3.amazonaws.com/dl.ncsbe.gov/data/PartyChange/2022_party_change_list.csv
+wget https://s3.amazonaws.com/dl.ncsbe.gov/data/PartyChange/2023_party_change_list.csv
+wget https://s3.amazonaws.com/dl.ncsbe.gov/data/PartyChange/2024_party_change_list.csv
+
+
